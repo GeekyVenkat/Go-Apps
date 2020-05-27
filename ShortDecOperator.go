719ba77e5dd941399af158a0df72b7afb41cef62
Venkat <venkat.adapa08@gmail.com>
2020-05-27 21:41:09 +0530

    Add files via upload

---
+++ ShortDecOperator.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	fmt.Println(x)
+	x = 90
+	fmt.Println(x)
+
+	y := 100 + 20
+	fmt.Println(y)
+
+	name := "Venkat Adapa"
+	fmt.Println(name)
+}
